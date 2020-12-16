55ac510cd92092c43a43612b7493c866de2d485f
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-12-16 00:03:40 -0300

    Desafio de código - Quanta Mandioca?

---
+++ introducao-programacao/quantaMandioca/quantaMandioca.js
@@ -0,0 +1,8 @@
+let chico = 300 * parseInt(gets());
+let bento = 1500 * parseInt(gets());
+let bernardo = 600 * parseInt(gets());
+let marina = 1000 * parseInt(gets());
+let iara = 150 * parseInt(gets());
+let marlene = 225;
+let total = 0; // Digite aqui o calculo total
+console.log(total);
