9fe8237729de03e2eceaf30a2b0f7db2a0bbc8b3
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-12-17 17:32:15 -0300

    Desafio de Código - Validacao de Notas - Resolvido

---
+++ solucao-de-problemas-essenciais/validacao-de-nota/validacaoDeNota.js
@@ -0,0 +1,18 @@
+
+let valid = 0;
+let media = 0;
+let soma = 0;
+
+while(valid < 2){
+  let nota = parseFloat(gets());
+  if(nota < 0 || nota > 10){
+    console.log('nota invalida')
+  }else if(nota >= 0 && nota <= 10){
+    valid = valid + 1;
+    soma = soma + nota;
+  }
+  media = soma / 2;
+}
+console.log("media = " + media.toFixed(2))
+
+
