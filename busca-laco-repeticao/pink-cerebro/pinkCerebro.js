4165b4881779ed9207c5c48d86fab32f57ebb9cd
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-12-23 12:48:29 -0300

    Busca e Laço de Repetição - Pink e Cérebro - resolvido

---
+++ busca-laco-repeticao/pink-cerebro/pinkCerebro.js
@@ -0,0 +1,21 @@
+let qtdNumero = parseInt(gets());
+let numeros = gets().split(" ");
+let multiplos = [2, 3, 4, 5];
+
+for (let index = 0; index < multiplos.length; index++) {
+
+    let contador = 0;
+    let multiplo = multiplos[index];
+
+    for (let index = 0; index < numeros.length; index++) {
+        let numero = numeros[index];
+
+        if(numero % multiplo == 0 ){
+            contador = contador + 1;
+        }
+        
+    }
+
+    console.log(contador + " Multiplo(s) de " + multiplo)
+
+}
